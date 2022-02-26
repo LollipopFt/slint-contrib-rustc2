27e911469c6155c999732c427bedcf970e9488c2
LollipopFt <lollipopz1426@gmail.com>
2022-02-26 14:12:27 +0800

    first commit.

---
+++ README.md
@@ -0,0 +1,30 @@
+This is a template. For "how to make a linter", please check [the HOWTO](HOWTO.md).
+
+-----------------------------------------------------------------
+
+slint-contrib-rustc2
+================================
+
+[![Build Status](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-rustc.svg?branch=master)](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-rustc)
+
+This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides an interface to [rustc](https://www.rust-lang.org/). It will be used with files that have the “rust” syntax.
+
+## Installation
+SublimeLinter must be installed in order to use this plugin.
+
+Please use [Package Control](https://packagecontrol.io) to install the linter plugin.
+
+Before installing this plugin, you must ensure that `rustc` is installed on your system.
+
+In order for `rustc` to be executed by SublimeLinter, you must ensure that its path is available to SublimeLinter. The docs cover [troubleshooting PATH configuration](http://sublimelinter.readthedocs.io/en/latest/troubleshooting.html#finding-a-linter-executable).
+
+## Settings
+- SublimeLinter settings: http://sublimelinter.readthedocs.org/en/latest/settings.html
+- Linter settings: http://sublimelinter.readthedocs.org/en/latest/linter_settings.html
+
+Additional SublimeLinter-contrib-rustc settings:
+
+|Setting|Description    |
+|:------|:--------------|
+|foo    |Something.     |
+|bar    |Something else.|
---
+++ linter.py
@@ -0,0 +1,149 @@
+import json
+import os
+import sys
+import SublimeLinter.lint as SLlint  # Linter, LintMatch, STREAM_STDERR
+
+
+class Rustc(SLlint.Linter):
+  '''rustc linter'''
+
+
+  cmd = (
+    'rustc', '--error-format=json', '--emit=mir', '-o', '/dev/null', '${file}'
+  )
+  defaults = {
+    'selector': 'source.rust'
+  }
+  error_stream = SLlint.STREAM_STDERR
+  name = 'rustc'
+  on_stderr = None
+
+  def find_errors(self, output):
+    '''function to find errors'''
+    if os.path.exists(os.path.join(self.working_dir, 'Cargo.toml')):
+      sys.exit()
+
+    lint_match = SLlint.LintMatch
+
+    for i in output.splitlines():
+      try:
+        compiled = json.loads(i)
+      except:
+        continue
+
+      mainmessage = compiled['message']
+
+      if compiled['code'] is None:
+        code = ''
+      else:
+        code = compiled['code']['code']
+
+      level = compiled['level']
+
+      spans = compiled['spans']
+      if spans is None:
+        yield lint_match(
+            filename=self.get.context('file'),
+            line=0,
+            end_line=1,
+            col=0,
+            end_col=1,
+            error_type=level,
+            code=code,
+            message=mainmessage
+        )
+      for spansobj in spans:
+        msg = mainmessage
+        if spansobj['label'] is not None:
+          if spansobj['label'] != '':
+            msg += '\n' + spansobj['label']
+        if spansobj['suggested_replacement'] is not None:
+          if spansobj['suggested_replacement'] != '':
+            msg += '\n' + spansobj['suggested_replacement']
+        if spansobj['suggestion_applicability'] is not None:
+          if spansobj['suggestion_applicability'] != '':
+            msg += ' ('+spansobj['suggestion_applicability']+')'
+        yield lint_match(
+          filename=spansobj['file_name'],
+          line=spansobj['line_start']-1,
+          end_line=spansobj['line_end']-1,
+          col=spansobj['column_start']-1,
+          end_col=spansobj['column_end']-1,
+          error_type=level,
+          code=code,
+          message=msg
+        )
+
+        if spansobj['is_primary'] is True:
+          for child in compiled['children']:
+            if not child['spans']:
+              if child['code'] is None:
+                code = ''
+              else:
+                code = child['code']
+              yield lint_match(
+                filename=spansobj['file_name'],
+                line=spansobj['line_start']-1,
+                end_line=spansobj['line_end']-1,
+                col=spansobj['column_start']-1,
+                end_col=spansobj['column_end']-1,
+                error_type=child['level'],
+                code=code,
+                message=child['message']
+              )
+
+        if spansobj['expansion'] is None:
+          continue
+        for span in spansobj['expansion']['span']:
+          msg = mainmessage
+          if span['label'] is not None:
+            if span['label'] != '':
+              msg += '\n' + span['label']
+          if span['suggested_replacement'] is not None:
+            if span['suggested_replacement'] != '':
+              msg += '\n' + span['suggested_replacement']
+          if span['suggestion_applicability'] is not None:
+            if span['suggestion_applicability'] != '':
+              msg += ' ('+span['suggestion_applicability']+')'
+          yield lint_match(
+            filename=span['file_name'],
+            line=span['line_start']-1,
+            end_line=span['line_end']-1,
+            col=span['column_start']-1,
+            end_col=span['column_end']-1,
+            error_type=level,
+            code=code,
+            message=msg
+          )
+
+      for child in compiled['children']:
+        if child['spans']:
+          mainmessage = child['message']
+          level = child['level']
+          if child['code'] is None:
+            code = ''
+          else:
+            code = child['code']['code']
+          level = child['warning']
+          spans = child['spans']
+          for spans in child['spans']:
+            msg = mainmessage
+            if spans['label'] is not None:
+              if spans['label'] != '':
+                msg += '\n' + spans['label']
+            if spans['suggested_replacement'] is not None:
+              if spans['suggested_replacement'] != '':
+                msg += '\n' + spans['suggested_replacement']
+            if spans['suggestion_applicability'] is not None:
+              if spans['suggestion_applicability'] != '':
+                msg += ' ('+spans['suggestion_applicability']+')'
+            yield lint_match(
+              filename=spans['file_name'],
+              line=spans['line_start']-1,
+              end_line=spans['line_end']-1,
+              col=spans['column_start']-1,
+              end_col=spans['column_end']-1,
+              error_type=level,
+              code=code,
+              message=msg
+            )
