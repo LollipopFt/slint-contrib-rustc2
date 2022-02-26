b0d5100437b2089af8d7dcf618ee258737b810e4
LollipopFt <lollipopz1426@gmail.com>
2022-02-26 20:53:54 +0800

    testing to see if Cargo.toml check works, fixed most of lints that plagued the file.

--- linter.py
+++ linter.py
@@ -1,6 +1,6 @@
 import json
-# import os
-# import sys
+import os
+import sys
 import SublimeLinter.lint as SLlint    # Linter, LintMatch, STREAM_STDERR
 
 
@@ -19,42 +19,20 @@ class Rustc(SLlint.Linter):
     on_stderr = None
     tempfile_suffix = 'rs'
 
+    def init(self):
+        '''check for Cargo.toml'''
+        if os.path.exists(os.path.join(self.working_dir, 'Cargo.toml')):
+            sys.exit()
+
     def find_errors(self, output):
         '''function to find errors'''
-        # if os.path.exists(os.path.join(self.working_dir, 'Cargo.toml')):
-        #     sys.exit()
 
-        lint_match = SLlint.LintMatch
+        def for_loop(spans_list, mainmessage, level, code, lint_match):
+            '''yield lints'''
 
-        for i in output.splitlines():
-            try:
-                compiled = json.loads(i)
-            except:
-                continue
-
-            mainmessage = compiled['message']
-
-            if compiled['code'] is None:
-                code = ''
-            else:
-                code = compiled['code']['code']
-
-            level = compiled['level']
-
-            spans = compiled['spans']
-            if spans is None:
-                yield lint_match(
-                        filename=self.get.context('file'),
-                        line=0,
-                        end_line=1,
-                        col=0,
-                        end_col=1,
-                        error_type=level,
-                        code=code,
-                        message=mainmessage
-                )
-            for spansobj in spans:
-                msg = mainmessage
+            def labelcheck(message, spansobj):
+                '''addons to msg'''
+                msg = message
                 if spansobj['label'] is not None:
                     if spansobj['label'] != '':
                         msg += '\n' + spansobj['label']
@@ -64,6 +42,10 @@ class Rustc(SLlint.Linter):
                 if spansobj['suggestion_applicability'] is not None:
                     if spansobj['suggestion_applicability'] != '':
                         msg += ' ('+spansobj['suggestion_applicability']+')'
+                return msg
+
+            for spansobj in spans_list:
+                msg = labelcheck(mainmessage, spansobj)
                 yield lint_match(
                     filename=spansobj['file_name'],
                     line=spansobj['line_start']-1,
@@ -74,7 +56,6 @@ class Rustc(SLlint.Linter):
                     code=code,
                     message=msg
                 )
-
                 if spansobj['is_primary'] is True:
                     for child in compiled['children']:
                         if not child['spans']:
@@ -92,30 +73,44 @@ class Rustc(SLlint.Linter):
                                 code=code,
                                 message=child['message']
                             )
-
                 if spansobj['expansion'] is None:
                     continue
-                for span in spansobj['expansion']['span']:
-                    msg = mainmessage
-                    if span['label'] is not None:
-                        if span['label'] != '':
-                            msg += '\n' + span['label']
-                    if span['suggested_replacement'] is not None:
-                        if span['suggested_replacement'] != '':
-                            msg += '\n' + span['suggested_replacement']
-                    if span['suggestion_applicability'] is not None:
-                        if span['suggestion_applicability'] != '':
-                            msg += ' ('+span['suggestion_applicability']+')'
-                    yield lint_match(
-                        filename=span['file_name'],
-                        line=span['line_start']-1,
-                        end_line=span['line_end']-1,
-                        col=span['column_start']-1,
-                        end_col=span['column_end']-1,
-                        error_type=level,
-                        code=code,
-                        message=msg
-                    )
+                yield from for_loop(
+                    spansobj['expansion']['span'],
+                    mainmessage, level, code, lint_match
+                )
+
+        lint_match = SLlint.LintMatch
+
+        for i in output.splitlines():
+            try:
+                compiled = json.loads(i)
+            except ValueError:
+                continue
+
+            mainmessage = compiled['message']
+
+            if compiled['code'] is None:
+                code = ''
+            else:
+                code = compiled['code']['code']
+
+            level = compiled['level']
+
+            spans = compiled['spans']
+            if spans is None:
+                yield lint_match(
+                    filename=self.get.context('file'),
+                    line=0,
+                    end_line=1,
+                    col=0,
+                    end_col=1,
+                    error_type=level,
+                    code=code,
+                    message=mainmessage
+                )
+
+            yield from for_loop(spans, mainmessage, level, code, lint_match)
 
             for child in compiled['children']:
                 if child['spans']:
@@ -127,25 +122,6 @@ class Rustc(SLlint.Linter):
                         code = child['code']['code']
                     level = child['level']
                     spans = child['spans']
-                    for spans in child['spans']:
-                        msg = mainmessage
-                        if spans['label'] is not None:
-                            if spans['label'] != '':
-                                msg += '\n' + spans['label']
-                        if spans['suggested_replacement'] is not None:
-                            if spans['suggested_replacement'] != '':
-                                msg += '\n' + spans['suggested_replacement']
-                        if spans['suggestion_applicability'] is not None:
-                            if spans['suggestion_applicability'] != '':
-                                msg += ' (' \
-                                    + spans['suggestion_applicability'] + ')'
-                        yield lint_match(
-                            filename=spans['file_name'],
-                            line=spans['line_start']-1,
-                            end_line=spans['line_end']-1,
-                            col=spans['column_start']-1,
-                            end_col=spans['column_end']-1,
-                            error_type=level,
-                            code=code,
-                            message=msg
-                        )
+                    yield from for_loop(
+                        spans, mainmessage, level, code, lint_match
+                    )
